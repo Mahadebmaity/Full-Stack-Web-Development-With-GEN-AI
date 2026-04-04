1fe26c3d4cefa6451948a971d8f624dd7200800e
MahadebBCA <163750294+Mahadebmaity@users.noreply.github.com>
2026-04-05 01:05:37 +0530

    create upto login register ui

---
+++ frontend/src/features/auth/pages/Register.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useNavigate,Link } from 'react-router';
+
+const handlesubmit = (e) => {
+    e.preventDefault();
+}
+
+const Register = () => {
+    const navigate = useNavigate();
+    return (
+        <main>
+            <div className="form-container">
+                <h2>Register</h2>
+                <form action="" onSubmit={handlesubmit}>
+
+
+                    <div className="input-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id='username' name='username' placeholder='Enter your username' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' name='email' placeholder='Enter your email' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' name='password' placeholder='Enter your password' />
+                    </div>
+                    <button className='button primary-button' type='submit'>Register</button>
+                </form>
+                <p>Already have an account? <Link to={"/login"}>Login</Link></p>
+            </div>
+        </main>
+    )
+}
+
+export default Register
---
+++ frontend/src/features/auth/pages/login.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../auth.form.scss'
+import { Link } from 'react-router';
+
+
+const login = () => {
+
+    const handlesubmit = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <main>
+            <div className="form-container">
+                <h2>Login</h2>
+                <form action="" onSubmit={handlesubmit}>
+                    <div className="input-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' placeholder='Enter your email' />
+                    </div>
+                    <div className="input-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' placeholder='Enter your password' />
+                    </div>
+                    <button className='button primary-button' type='submit'>Login</button>
+                </form>
+                <p>Already have an account? <Link to={"/register"}>Register</Link></p>
+            </div>
+        </main> 
+    )
+}
+
+export default login
