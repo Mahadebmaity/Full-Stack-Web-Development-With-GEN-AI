b3a7c0b1268a9d76d7d2e861f8e0a5655e8bc1d1
MahadebBCA <163750294+Mahadebmaity@users.noreply.github.com>
2026-04-06 00:09:27 +0530

    update and install dependencies with change some code

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require("express") //Import the Express library to create a web application server 
 const cookieParser = require("cookie-parser") // Import the cookie-parser middleware to handle cookies in incoming requests
+const cors = require("cors") // Import the CORS middleware to enable Cross-Origin Resource Sharing
+
 
 const app = express(); // Create an instance of the Express application 
 
 app.use(express.json()); // Middleware to parse JSON bodies in incoming requests
 app.use(cookieParser()); // Middleware to parse cookies in incoming requests
+app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from this origin (frontend)
+    credentials: true // Allow cookies to be sent in cross-origin requests
+}))
 
 const authRouter = require("./routes/auth.routes") // Import the authentication routes from the specified file
 // required all the routes here and use them in the app
